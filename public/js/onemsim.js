0fbc5ddca1e9347d1ffe3385433a80ae63ed6698
Chris Horn <chrishornuk@gmail.com>
2016-12-19 08:54:32 +0000

    dummy mo and mt

--- public/js/onemsim.js
+++ public/js/onemsim.js
@@ -87,6 +87,21 @@ ONEmSimModule.config(['$httpProvider',
     }
 ]);
 
+ONEmSimModule.directive('scrollBottom', function() {
+    return {
+        scope: {
+            scrollBottom: "="
+        },
+        link: function(scope, element) {
+            scope.$watchCollection('scrollBottom', function(newValue) {
+                if (newValue) {
+                    $(element).scrollTop($(element)[0].scrollHeight);
+                }
+            });
+        }
+    };
+});
+
 ONEmSimModule.controller('mainController', [
     '$scope',
     '$http',
