3a3f6e31d04708eb1d0379a63b8267e977e92f4f
Chris Horn <chrishornuk@gmail.com>
2016-12-20 21:15:41 +0000

    refactored code

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(session({
     secret: '0nems1m',
     resave: false,
     saveUninitialized: true,
-    cookie: { maxAge: 24 * 360000 }  // 24 hours
+    cookie: { maxAge: 24 * 360000 } // 24 hours
 }));
 
 // Use the API routes when path starts with /api
@@ -56,9 +56,6 @@ function getMenuResponse(input, menu) {
     for (var i = 0; i < menu.length; i++) {
         response = response + menuOptions[i] + ' ' + menu[i].description + '\n';
     }
-
-    // make footer for menu
-    response = response + menuFooter;
 
     return response;
 }
@@ -119,64 +116,117 @@ function processRequest(input, context) {
 
 }
 
+function validateInput(moText, context) {
+
+    var input = moText.toLowerCase();
+    var response = { success: true };
+
+    switch (context.type) {
+        case 'menu':
+            var menuContent = context.content;
+
+            // if it's only 1 char long and in range of a to last menu option
+            var found = menuOptions.indexOf(input[0]);
+            if (input.length === 1 &&
+                found !== -1 &&
+                found >= menuContent.length &&
+                found <= menuContent.length) {
+                response.success = true;
+                response.firstOption = 'a';
+                response.lastOption = menuOptions[menuContent.length];
+            } else {
+                response.success = false;
+            }
+            break;
+        default:
+            break;
+
+    }
+
+    return response;
+
+}
+
+function processHeader(context) {
+    var header = '';
+    if (typeof context.header !== 'undefined') {
+        header = context.header + '\n';
+    }
+    return header;
+}
+
+function processFooter(context) {
+    return menuFooter;
+}
+
+
 app.get('/api/getResponse', function(req, res) {
 
-    var moText = req.query.moText.trim().toLowerCase();
+    var moText = req.query.moText.trim();
     var response = '';
     var header = '';
-    var session = req.session;
+    var footer = '';
+    var firstChar = '';
+    var firstTime = false;
+    var context = req.session.onemContext;
+    var verb = false;
+    var status = {success : true };
 
-    console.log(session);
+    console.log(req.session);
 
     if (moText.length === 0) return res.json({ mtText: '' });
 
-    if (typeof session.onemContext === 'undefined') {  // must be first time, or expired
-
-        var index = JSON.parse(fs.readFileSync('./app_api/json/index.json', 'utf8'));
-        console.log("index.content.length:" + index.content.length);
-
-        if (typeof index !== 'undefined') {
-            session.onemContext = {
-                fileName: 'onem.json',
-                savedContext: JSON.parse(JSON.stringify(index.content[0]))
-            };
-            console.log("storing context");
-        }
+    if (typeof context === 'undefined') { // must be first time, or expired
+        console.log("First time login or session expired");
+        req.session.onemContext = { fileName :'onem.json' };
+        context = req.session.onemContext;
+        context.savedContext = {};
         moText = "#onem";
+        firstTime = true;
     }
 
-    // check MO request
+    firstChar = moText[0].toLowerCase();
 
+    // check MO request for reserved verbs or service switching
     switch (true) {
-        case (moText[0] == '#'):
+        case (firstChar == '#'):
             console.log("service switch");
-            session.onemContext.savedContext = serviceSwitch(session.onemContext.savedContext, moText);
-            break;
-        case (moText[0] >= 'a' && moText[0] <= 'z'):
-        case (moText[0] >= '1' && moText[0] <= '9'):
-            console.log("menu option");
+            context.savedContext = serviceSwitch(context.savedContext, moText);
+            verb = true;
             break;
         case (moText === 'menu'):
+            console.log("menu selected");
+            verb = true;
+            break;
+        case (moText === 'back'):
+            console.log("back selected");
+            verb = true;
             break;
         default:
             console.log("other");
             break;
     }
 
-    console.log("before");
-    console.log(session.onemContext.savedContext);
-
-
-    if (typeof session.onemContext.savedContext.header !== 'undefined') {
-        header = session.onemContext.savedContext.header + '\n';
+    // if it's not a reserved word, then validate the input against the savedContext
+    // eg if it's a menu option, check the selected option value
+    // if it's an input string, then accept anything else (other than a reserved verb)
+    if (!verb && !firstTime) {
+        status = validateInput(moText, context.savedContext);
     }
 
-    response = processRequest(moText, session.onemContext.savedContext);
+    response = processRequest(moText, context.savedContext);
 
-    console.log("after");
-    console.log(session.onemContext.savedContext);
+    console.log("status.success:"+status.success);
 
-    var finalResponse = header + response;
+    if (status.success) {
+        header = processHeader(context.savedContext);
+        footer = processFooter(context.savedContext);
+    }
+
+//    console.log("after");
+//    console.log(context.savedContext);
+
+    var finalResponse = header + response + footer;
 
     res.json({ mtText: finalResponse });
 });
