44b7af3e2ec04a7ee22b1022ccfb05424f8a8bb7
Chris Horn <chrishornuk@gmail.com>
2017-03-20 16:35:39 +0000

    trying to fix receive submit_sm

--- app.js
+++ app.js
@@ -135,7 +135,7 @@ app.get('/api/getResponse', function(req, res, next) {
 
     smppSession.on('submit_sm', function(pdu) {
         //  var msgid = getMsgId(); // generate a message_id for this message.
-        console.log("submit_sm received, msgid:" + msgid);
+        console.log("submit_sm received");
         smppSession.send(pdu.response({
             //   message_id: msgid
         }));
