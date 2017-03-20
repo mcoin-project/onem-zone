46a4cdf8074e99d2471c086ff7f842a6dc2a4e16
Chris Horn <chrishornuk@gmail.com>
2017-03-20 12:54:51 +0000

    first version

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,6 +1,5 @@
 var _ = require('underscore-node');
 var express = require('express');
-var mongoose = require('bluebird').promisifyAll(require('mongoose'));
 
 var api = express.Router();
 module.exports = api;
