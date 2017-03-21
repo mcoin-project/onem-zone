08afefbbcf108b9171f207cb78287547dbb093aa
Chris Horn <chrishornuk@gmail.com>
2017-03-21 10:30:16 +0200

    working tests

--- tests/onem.js
+++ tests/onem.js
@@ -1,7 +1,37 @@
 describe('onem test suite', function() {
-  it('should send #onem', function() {
-    browser.get('http://172.16.38.98:5000');
-
-    element(by.id('sms')).sendKeys('#onem');
-  });
+    it('should send #onem', function() {
+        browser.get('http://172.16.38.98:5000').then(function() {
+            element(by.id('sms')).sendKeys('#onem');
+            element(by.id('sms')).sendKeys(protractor.Key.ENTER).then(function() {
+                element.all(by.repeater('obj in results')).then(function(mt) {
+                    var message = mt[mt.length - 1].element(by.className('sms-mt-container'));
+                    expect(message.getText()).toEqual(
+                        '** ONEm menu **\n' +
+                        'a My account\n' +
+                        'b Invite friends to chat for free\n' +
+                        'c Create free SMS group chats (xGroup)\n' +
+                        'd Free services\n' +
+                        'e How to use\n' +
+                        '<send option>'
+                    );
+                });
+            });
+        });
+    });
+    it('should send #post search', function() {
+        browser.get('http://172.16.38.98:5000').then(function() {
+            element(by.id('sms')).sendKeys('#post search');
+            element(by.id('sms')).sendKeys(protractor.Key.ENTER).then(function() {
+                element.all(by.repeater('obj in results')).then(function(mt) {
+                    var message = mt[mt.length - 1].element(by.className('sms-mt-container'));
+                    expect(message.getText()).toEqual(
+                        'POST mode activated\n' +
+                        '** Post search **\n' +
+                        'Send post code, user name, any keyword or \"back\"\n' +
+                        '<\"back\">'
+                    );
+                });
+            });
+        });
+    });
 });
