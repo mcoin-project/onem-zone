db3e5f3f4d6e1713e0eafebfe859e44fb920de5a
Chris Horn <chrishornuk@gmail.com>
2017-03-21 11:08:13 +0200

    debug

--- app.js
+++ app.js
@@ -140,6 +140,8 @@ app.get('/api/getResponse', function(req, res, next) {
         //  var msgid = getMsgId(); // generate a message_id for this message.
         console.log("submit_sm received, sequence_number:" + pdu.sequence_number + " isResponse:" + pdu.isResponse());
 
+        console.log(pdu);
+
         // smppSession.send(pdu.response({
         //     sequence_number: pdu.sequence_number
         //          message_id: msgid
