6f32fdffa02209dcbd2f494950c6af52c516ad9a
Mihai Titianu <mihai.titianu@onem.com>
2017-06-29 17:01:27 +0000

    Improving UI and SIP

--- public/js/onemsim.js
+++ public/js/onemsim.js
@@ -107,6 +107,32 @@ ONEmSimModule.directive('scrollBottom', function() {
     };
 });
 
+ONEmSimModule.directive('myClock', function($interval, dateFilter) {
+    return {
+        restrict: "A",
+        transclude: true,
+        scope: {
+            format: "@"
+        },
+        link: function(scope, element, attrs) {
+            var format = scope.clock || 'HH:mm:ss';
+
+            var updateTime = function() {
+                element.text(dateFilter(new Date(),format));
+            };
+
+            //Schedule update every second:
+            var timer = $interval(updateTime, 1000);
+
+            //Listen on DOM destroy (removal) event and cancel the next UI update
+            //to prevent updating time after the DOM element was removed:
+            element.on('$destroy', function() {
+                $interval.cancel(timer);
+            });
+        }
+    };
+});
+
 ONEmSimModule.factory('DataModel', function() {
     var data = {
         tabs: [
@@ -175,51 +201,15 @@ ONEmSimModule.controller('mainController', [
     'Socket',
     function($scope, $http, SmsHandler, DataModel, Socket) {
 
+        var isInCall = 0;
+
         console.log("mainController initialising");
-
-        $('a.full').click(function(e){
-            e.preventDefault();
-            $('.phone').toggleClass('full');
-            $('body').toggleClass('full');
-            $(this).toggleClass('toggled')
-            return false;
-        });
-
-        $('a.open_dialer').click(function(e) {
-            e.preventDefault();
-            $(this).parents('.phone').find('div.dialer').toggleClass('open');
-            console.log('[UI]: Dialer state changed!');
-            return false;
-        });
-
-        $('a.num').click(function(e){
-            e.preventDefault();
-            var $btn = $(this);
-            var val = $(this).data('val');
-            $btn.addClass('pressed');
-            setTimeout(function () {
-                $btn.removeClass('pressed');
-            }, 400);
-            $('.dialer #typed_no').val($('.dialer #typed_no').val() + val);
-            return false;
-        });
-
-        $('a.delete').click(function(e){
-            e.preventDefault();
-            var $btn = $(this);
-            $btn.addClass('pressed');
-            setTimeout(function () {
-                $btn.removeClass('pressed');
-            }, 400);
-            $('.dialer #typed_no').val($('.dialer #typed_no').val().slice(0,-1));
-            return false;
-        });
 
         //These are the buttons of the phone's user interface:
         var AnswerButton = $('.call_tools a.answer');
         var RejectButton = $('.call_tools a.cancel');
         var CallButton = $('.screen a.call');
-        var ClosePanelButton = $('.screen a.close');
+        var ClosePanelButton = $('.screen a.closer');
 
         var audioElement = document.getElementById('myAudio');
         console.log(audioElement);
@@ -234,7 +224,10 @@ ONEmSimModule.controller('mainController', [
             'failed'    : function(e) {
                 console.log('eventHandlers - failed');
                 audioElement.pause();
+                isInCall = 0;
                 $('.phone div.panel').removeClass('open');
+                $('.phone .call_notif').removeClass('on');
+                $('.answer ul.nums').removeClass('on');
                 $('.answer #typed_no').val('');
                 $('.dialer #typed_no').val('');
                 $('.caller #typed_no').val('');
@@ -242,7 +235,10 @@ ONEmSimModule.controller('mainController', [
             'ended'     : function(e) {
                 console.log('eventHandlers - ended');
                 audioElement.pause();
+                isInCall = 0;
                 $('.phone div.panel').removeClass('open');
+                $('.phone .call_notif').removeClass('on');
+                $('.answer ul.nums').removeClass('on');
                 $('.answer #typed_no').val('');
                 $('.dialer #typed_no').val('');
                 $('.caller #typed_no').val('');
@@ -250,8 +246,10 @@ ONEmSimModule.controller('mainController', [
             'confirmed' : function(e) {
                 console.log('eventHandlers - confirmed');
                 //RTCPeerConnection.getLocalStreams/getRemoteStreams are deprecated. Use RTCPeerConnection.getSenders/getReceivers instead.
-                audioElement.src = window.URL.createObjectURL(globalSession.connection.getRemoteStreams()[0]);
+                //audioElement.src = window.URL.createObjectURL(globalSession.connection.getRemoteStreams()[0]);
+                audioElement.srcObject = globalSession.connection.getRemoteStreams()[0];
                 audioElement.play();
+                isInCall = 1;
             },
             'addstream' : function(e) {
                 console.log('eventHandlers - addstream');
@@ -259,15 +257,30 @@ ONEmSimModule.controller('mainController', [
         };
 
         var options = {
-            'eventHandlers'        : eventHandlers,
-            'sessionTimersExpires' : 600,
-            'pcConfig'             : {
-                'rtcpMuxPolicy'    : 'negotiate',
-                'iceServers'       : [
-                    { 'urls'       : [ 'stun:stun.l.google.com:19302' ] }
+            'eventHandlers'          : eventHandlers,
+            'sessionTimersExpires'   : 600,
+            'session_timers'         : true,
+            'use_preloaded_route'    : false,
+            'pcConfig'               : {
+                'rtcpMuxPolicy'      : 'negotiate',
+                'iceServers'         : // [ {
+                //        'urls'       : 'stun:stun.l.google.com:19302'
+                //    }, {
+                //        'urls'       : 'turn:192.158.29.39:3478?transport=udp',
+                //        'credential' : 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                //        'username'   : '28224511:1379330808'
+                //    }, {
+                //        'urls'       : 'turn:192.158.29.39:3478?transport=tcp',
+                //        'credential' : 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                //        'username'   : '28224511:1379330808'
+                //    }
+                //]
+                [
+                    { 'urls'         : [ 'stun:stun.l.google.com:19302' ] }
+                    //{ 'urls'         : [ 'stun:stunserver.org' ] }
                 ]
             },
-            'mediaConstraints'     : { 'audio' : true, 'video' : false }
+            'mediaConstraints'       : { 'audio' : true, 'video' : false }
         };
 
         var startResponse = SmsHandler.start({}, function() {
@@ -289,35 +302,152 @@ ONEmSimModule.controller('mainController', [
 
             var phoneONEm = new JsSIP.UA(configuration);
 
+            $('a.full').click(function(e){
+                e.preventDefault();
+                $('.phone').toggleClass('full');
+                $('body').toggleClass('full');
+                $(this).toggleClass('toggled')
+                return false;
+            });
+
+            $('a.open_dialer').click(function(e) {
+                e.preventDefault();
+                $(this).parents('.phone').find('div.dialer').toggleClass('open');
+                console.log('[UI]: Dialer state changed!');
+                return false;
+            });
+
+            $('.answer a.num').click(function(e){
+                e.preventDefault();
+                var $btn = $(this);
+                var val = $(this).data('val');
+                $btn.addClass('pressed');
+                setTimeout(function () {
+                    $btn.removeClass('pressed');
+                }, 400);
+                globalSession.sendDTMF(val);
+                console.log("Sending DTMF " + val);
+                return false;
+            });
+
+            $('.dialer a.num').click(function(e){
+                e.preventDefault();
+                var $btn = $(this);
+                var val = $(this).data('val');
+                var resizeTextarea = function(el) {
+                    var offset = el[0].offsetHeight - el[0].clientHeight;
+                    $(el).css('height', 'auto').css('height', el[0].scrollHeight + offset);
+                };
+                $btn.addClass('pressed');
+                setTimeout(function () {
+                    $btn.removeClass('pressed');
+                }, 400);
+                $('.dialer #typed_no').val($('.dialer #typed_no').val() + val);
+                resizeTextarea($('.dialer #typed_no'));
+                return false;
+            });
+
+            $('a.delete').click(function(e){
+                e.preventDefault();
+                var $btn = $(this);
+                $btn.addClass('pressed');
+                //setTimeout(function () {
+                //    $btn.removeClass('pressed');
+                //}, 400);
+                $('.dialer #typed_no').val($('.dialer #typed_no').val().slice(0,-1));
+                return false;
+            });
+
+            $.each($('textarea[data-autoresize]'), function() {
+                var offset = this.offsetHeight - this.clientHeight;
+                var resizeTextarea = function(el) {
+                    $(el).css('height', 'auto').css('height', el.scrollHeight + offset);
+                };
+                $(this).on('keyup input', function(e) { 
+                    resizeTextarea(this); 
+                }).removeAttr('data-autoresize');
+            });
+
+            $('textarea').keypress(function(e) {
+                var a = [];
+                var k = e.which;
+                a.push(8);
+                a.push(42);
+                a.push(43);
+                for (var i = 48; i < 58; i++) a.push(i);
+                if (!(a.indexOf(k)>=0)) e.preventDefault();
+            });
+
+            $('a.numpad').click(function(e) {
+                e.preventDefault();
+                $('.answer ul.nums').toggleClass('on');
+                console.log('[UI]: In call numpad state changed');
+                return false;
+            });
+
+            $('.answer a.minimize').click(function(e) {
+                e.preventDefault();
+                $('.call_notif').addClass('on');
+                $('.phone div.panel').removeClass('open');
+                console.log('[UI]: Pannels minimized from answer panel');
+                return false;
+            });
+
+            $('.call_notif a.resume').click(function(e) {
+                e.preventDefault();
+                $('.call_notif').removeClass('on');
+                $('.phone div.panel.answer').addClass('open');
+                console.log('[UI]: Answer panel maximized');
+                return false;
+            });
+
             phoneONEm.start();
 
             phoneONEm.on('newRTCSession', function(data){
                 console.log('newRTCSession');
                 globalSession = data.session; //session pointer
+                //console.log(globalSession);
 
                 $('.phone div.caller').addClass('open');
 
                 //Play ring tone:
-                audioElement.src = "/sounds/old_british_phone.wav";
+                if(globalSession.direction === "incoming") {
+                    //Incoming call; play ring
+                    console.log("Load incoming call ring:");
+                    audioElement.src = "/sounds/old_british_phone.wav";
+                } else {
+                    //Outgoing call; play ringback tone
+                    console.log("Load outgoing callback tone:");
+                    audioElement.src = "/sounds/ringing_tone_uk_new.wav";
+                };
+
+                console.log("Play the loaded sound:");
                 audioElement.play();
              
                 //originator
                 console.log('Caller ID: ' + globalSession.remote_identity.uri.user);
+                console.log('User Name: ' + globalSession.remote_identity.display_name);
                 $('.answer #typed_no').val(globalSession.remote_identity.uri.user);
                 $('.caller #typed_no').val(globalSession.remote_identity.uri.user);
+                $scope.usr_name = globalSession.remote_identity.display_name;
 
                 if(globalSession.direction === "incoming"){
                     //incoming call here:
                     globalSession.on("accepted",function(){
                         console.log('newRTCSession - incoming - accepted');
-                        //RTCPeerConnection.getLocalStreams/getRemoteStreams are deprecated. Use RTCPeerConnection.getSenders/getReceivers instead.
-                        audioElement.src = window.URL.createObjectURL(globalSession.connection.getRemoteStreams()[0]);
+                        ////RTCPeerConnection.getLocalStreams/getRemoteStreams are deprecated. Use RTCPeerConnection.getSenders/getReceivers instead.
+                        //audioElement.src = window.URL.createObjectURL(globalSession.connection.getRemoteStreams()[0]);
+                        audioElement.srcObject = globalSession.connection.getRemoteStreams()[0];
                         audioElement.play();
+                        isInCall = 1;
                     });
                     globalSession.on("ended",function(e){
                         console.log('newRTCSession - incoming - ended');
                         audioElement.pause();
+                        isInCall = 0;
                         $('.phone div.panel').removeClass('open');
+                        $('.phone .call_notif').removeClass('on');
+                        $('.answer ul.nums').removeClass('on');
                         $('.answer #typed_no').val('');
                         $('.dialer #typed_no').val('');
                         $('.caller #typed_no').val('');
@@ -325,7 +455,10 @@ ONEmSimModule.controller('mainController', [
                     globalSession.on("failed",function(e){
                         console.log('newRTCSession - incoming - failed');
                         audioElement.pause();
+                        isInCall = 0;
                         $('.phone div.panel').removeClass('open');
+                        $('.phone .call_notif').removeClass('on');
+                        $('.answer ul.nums').removeClass('on');
                         $('.answer #typed_no').val('');
                         $('.dialer #typed_no').val('');
                         $('.caller #typed_no').val('');
@@ -345,6 +478,7 @@ ONEmSimModule.controller('mainController', [
                 globalSession.answer(options);
                 $('.phone div.panel').removeClass('open');
                 $('.phone div.answer').addClass('open');
+                isInCall = 1;
             });
 
             // End the call or reject the call:
@@ -357,6 +491,7 @@ ONEmSimModule.controller('mainController', [
             CallButton.click( function(){
                 console.log('CallButton - click; Call to ' + $('.dialer #typed_no').val());
                 phoneONEm.call('sip:' + $('.dialer #typed_no').val() + '@' + sipProxy, options);
+                isInCall = 1;
                 $('.answer #typed_no').val( $('.dialer #typed_no').val() );
                 $('.dialer #typed_no').val('');
                 $('.phone div.panel').removeClass('open');
@@ -366,6 +501,8 @@ ONEmSimModule.controller('mainController', [
             ClosePanelButton.click(function(e){
                 console.log('ClosePanelButton - click');
                 $('.phone div.panel').removeClass('open');
+                if(globalSession) globalSession.terminate();
+                if(isInCall==1) $('.phone div.answer').toggleClass('open');
             });
 
             //function IncomingEndCall() {
