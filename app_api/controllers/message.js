73eb5509526ff53ea207ac961d8d158e7d47912e
Chris Horn <chrishornuk@gmail.com>
2018-12-23 14:28:32 +0000

    retrieving messages

--- app_api/controllers/message.js
+++ app_api/controllers/message.js
@@ -8,10 +8,12 @@ var User = mongoose.model('users', UserSchema);
 var MessageSchema = require('../models/Model').MessageSchema;
 var Message = mongoose.model('messages', MessageSchema);
 
-exports.save = function(from, to, text) {
+var bluebird = require('bluebird');
+
+exports.save = function (from, to, text) {
     console.log("messages.save");
     console.log("from: " + from + ' to:' + to);
-    common.getUser(to).then(function(user) {
+    common.getUser(to).then(function (user) {
         var message = new Message();
         message._user = user._id;
         message.from = from;
@@ -19,10 +21,27 @@ exports.save = function(from, to, text) {
         message.text = text;
         message.delivered = false;
         return message.save();
-    }).catch(function(error) {
+    }).catch(function (error) {
         console.log("/message.save");
         console.log(error);
         return new Error(error);
     });
 }
 
+exports.deliverPending = function (socket) {
+    return new Promise(function (resolve, reject) {
+        if (!socket.msisdn) {
+            console.log("msisdn missing");
+            return reject("msisdn missing");
+        }
+        common.getUser(socket.msisdn).then(function (user) {
+            Message.updateMany({ _user: user._id, delivered: false }, { $set: { delivered: true } }).then(function (users) {
+                users.map(function (user) {
+                    socket.emit('MT SMS', { mtText: user.text });
+                });
+            }).catch(function (error) {
+                reject(error);
+            })
+        })
+    });
+}
