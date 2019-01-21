3a53f942df416000da96b1929fac303e9d8fd0d3
Chris Horn <chrishornuk@gmail.com>
2019-01-21 10:04:33 +0000

    testing close keyboard

--- app_client/js/directives/closeKeyboard.js
+++ app_client/js/directives/closeKeyboard.js
@@ -1,36 +1,38 @@
-ONEmSimModule.directive('closeKeyboard1', [
-    '$window',
-    function ($window) {
-        return function (scope, element, attr) {
+// ONEmSimModule.directive('closeKeyboard1', [
+//     '$window',
+//     function ($window) {
+//         return function (scope, element, attr) {
 
-            var textFields = element.find('input');
-            element.bind('submit', function () {
+//             var textFields = element.find('input');
+//             element.bind('submit', function () {
 
-                /* To dismiss onscreen keyboard */
-                // In some cases, focus was needed before blur to dismiss onscreen keyboard
-                textFields[0].focus();
-                textFields[0].blur();
+//                 /* To dismiss onscreen keyboard */
+//                 // In some cases, focus was needed before blur to dismiss onscreen keyboard
+//                 textFields[0].focus();
+//                 textFields[0].blur();
 
-                /* To ensure status messages are visible on small screens */
-                $window.scrollTo(0, 0);
-            });
-        }
-    }
-]);
+//                 /* To ensure status messages are visible on small screens */
+//                 $window.scrollTo(0, 0);
+//             });
+//         }
+//     }
+// ]);
 
 ONEmSimModule.directive('closeKeyboard', [
     function() {
         return {
             restrict: 'A',
-            link: function(scope, elem, attrs) {
-                var textFields = element.find('input');
+            link: function(scope, element, attrs) {
+                //var textFields = element.find('input');
 
-                elem.bind('keyup', function(event) {
+                element.bind('keyup', function(event) {
                     if (event.keyCode === 13) {
+                        console.log("13 event");
                         /* To dismiss onscreen keyboard */
                         // In some cases, focus was needed before blur to dismiss onscreen keyboard
                         //textFields[0].focus();
-                        textFields[0].blur();
+                        //textFields[0].blur();
+                        element[0].blur();
         
                         /* To ensure status messages are visible on small screens */
                         //$window.scrollTo(0, 0);
