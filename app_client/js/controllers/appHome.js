3705e3f5962eaea0e617828e268c8025f3aeecc2
Chris Horn <chrishornuk@gmail.com>
2019-02-11 12:38:23 +0000

    bug fix

--- app_client/js/controllers/appHome.js
+++ app_client/js/controllers/appHome.js
@@ -8,6 +8,49 @@ ONEmSimModule.controller('appHomeController', [
     '$rootScope',
     function ($scope, SmsHandler, Cache, Phone, $state, $rootScope) {
 
+        $scope.$on('error', function (ev, data) {
+            console.log("[MN]: socket error:" + ev);
+            console.log(ev);
+            console.log(data);
+        });
+
+        $scope.$on('socket:LOGOUT', function (ev, data) {
+            $location.path('/logout');
+            $timeout(function () {
+                // anything you want can go here and will safely be run on the next digest.
+                $rootScope.$apply();
+            });
+        });
+
+        $scope.$on('socket:MT SMS', function (ev, data) {
+            $scope.theData = data;
+
+            console.log("[MN]: MT received:");
+            console.log(data);
+
+            var outputObj = {
+                type: "mt",
+                value: data.mtText
+            };
+
+            $scope.results = DataModel.addResult(outputObj);
+
+        });
+
+        $scope.smsInput = function () {
+
+            if (typeof $scope.smsText === 'undefined' || $scope.smsText.length === 0) return;
+
+            var inputObj = {
+                type: "mo",
+                value: $scope.smsText
+            };
+            $scope.results = DataModel.addResult(inputObj);
+            console.log("[MN]: calling emit");
+            Socket.emit('MO SMS', $scope.smsText);
+            $scope.smsText = '';
+        };
+
         SmsHandler.start().$promise.then(function (response) {
             console.log("response fom smshandler.start");
             console.log(response);
--- app_client/js/controllers/main.js
+++ app_client/js/controllers/main.js
@@ -17,49 +17,6 @@ ONEmSimModule.controller('mainController', [
 
         console.log("[MN]: mainController initialising");
 
-        $scope.$on('error', function (ev, data) {
-            console.log("[MN]: socket error:" + ev);
-            console.log(ev);
-            console.log(data);
-        });
-
-        $scope.$on('socket:LOGOUT', function (ev, data) {
-            $location.path('/logout');
-            $timeout(function () {
-                // anything you want can go here and will safely be run on the next digest.
-                $rootScope.$apply();
-            });
-        });
-
-        $scope.$on('socket:MT SMS', function (ev, data) {
-            $scope.theData = data;
-
-            console.log("[MN]: MT received:");
-            console.log(data);
-
-            var outputObj = {
-                type: "mt",
-                value: data.mtText
-            };
-
-            $scope.results = DataModel.addResult(outputObj);
-
-        });
-
-        $scope.smsInput = function () {
-
-            if (typeof $scope.smsText === 'undefined' || $scope.smsText.length === 0) return;
-
-            var inputObj = {
-                type: "mo",
-                value: $scope.smsText
-            };
-            $scope.results = DataModel.addResult(inputObj);
-            console.log("[MN]: calling emit");
-            Socket.emit('MO SMS', $scope.smsText);
-            $scope.smsText = '';
-        };
-
         Promise.resolve().then(function () {
             if (!$rootScope.msisdn) {
                 return User.getMsisdn().$promise;
