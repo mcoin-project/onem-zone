c7f05d03e44ef023f108c0ca89913934e03f93b1
Chris Horn <chrishornuk@gmail.com>
2019-02-12 17:01:39 +0000

    trying async/await

--- app_client/js/controllers/appHome.js
+++ app_client/js/controllers/appHome.js
@@ -11,51 +11,6 @@ ONEmSimModule.controller('appHomeController', [
     '$location',
     function ($scope, SmsHandler, Cache, Phone, DataModel, Socket, $state, $rootScope, $location) {
 
-        $scope.$on('error', function (ev, data) {
-            console.log("[MN]: socket error:" + ev);
-            console.log(ev);
-            console.log(data);
-        });
-
-        $scope.$on('socket:LOGOUT', function (ev, data) {
-            $location.path('/logout');
-            $timeout(function () {
-                // anything you want can go here and will safely be run on the next digest.
-                $rootScope.$apply();
-            });
-        });
-
-        $scope.$on('socket:MT SMS', function (ev, data) {
-            $scope.theData = data;
-
-            console.log("[MN]: MT received:");
-            console.log(data);
-
-            var outputObj = {
-                type: "mt",
-                value: data.mtText
-            };
-
-            $scope.results = DataModel.addResult(outputObj);
-
-        });
-
-
-
-        $scope.smsInput = function () {
-
-            if (typeof $scope.smsText === 'undefined' || $scope.smsText.length === 0) return;
-
-            var inputObj = {
-                type: "mo",
-                value: $scope.smsText
-            };
-            $scope.results = DataModel.addResult(inputObj);
-            console.log("[MN]: calling emit");
-            Socket.emit('MO SMS', $scope.smsText);
-            $scope.smsText = '';
-        };
-
         SmsHandler.start().$promise.then(function (response) {
             console.log("response fom smshandler.start");
             console.log(response);
--- app_client/js/controllers/nav.js
+++ app_client/js/controllers/nav.js
@@ -1,11 +1,14 @@
 ONEmSimModule.controller('navbarController', [
     '$scope',
     '$rootScope',
-    '$templateCache',
+    '$timeout',
     '$auth',
     '$state',
+    '$location',
     'Cache',
-    function ($scope, $rootScope, $templateCache, $auth, $state, Cache) {
+    'DataModel',
+    'Socket',
+    function ($scope, $rootScope, $timeout, $auth, $state, $location, Cache, DataModel, Socket) {
         $scope.isAuthenticated = function () {
             return $auth.isAuthenticated();
         }
@@ -13,6 +16,49 @@ ONEmSimModule.controller('navbarController', [
         $scope.agree = function() {
             $state.go('logoutDelete');
         }
+
+        $scope.$on('error', function (ev, data) {
+            console.log("[MN]: socket error:" + ev);
+            console.log(ev);
+            console.log(data);
+        });
+
+        $scope.$on('socket:LOGOUT', function (ev, data) {
+            $location.path('/logout');
+            $timeout(function () {
+                // anything you want can go here and will safely be run on the next digest.
+                $rootScope.$apply();
+            });
+        });
+
+        $scope.$on('socket:MT SMS', function (ev, data) {
+            $scope.theData = data;
+
+            console.log("[MN]: MT received:");
+            console.log(data);
+
+            var outputObj = {
+                type: "mt",
+                value: data.mtText
+            };
+
+            $scope.results = DataModel.addResult(outputObj);
+
+        });
+
+        $scope.smsInput = function () {
+
+            if (typeof $scope.smsText === 'undefined' || $scope.smsText.length === 0) return;
+
+            var inputObj = {
+                type: "mo",
+                value: $scope.smsText
+            };
+            $scope.results = DataModel.addResult(inputObj);
+            console.log("[MN]: calling emit");
+            Socket.emit('MO SMS', $scope.smsText);
+            $scope.smsText = '';
+        };
 
         $scope.$on('socket:API MT SMS', function (ev, data) {
             console.log("getService: received MT");
@@ -22,31 +68,5 @@ ONEmSimModule.controller('navbarController', [
 
         });
 
-        // $rootScope.$watch('user', function (newVal, oldVal, scope) {
-
-        //     console.log("newVal");
-        //     console.log(newVal);
-
-        //     console.log("oldVal");
-        //     console.log(oldVal);
-
-        //     if (JSON.stringify(newVal) == JSON.stringify(oldVal)) return;
-
-        //     console.log("user changed:");
-
-        //     var currentPageTemplate;
-        //     if ($state.current) {
-        //         //if ($state.current.templateUrl == 'partials/index.html') {
-        //         //    $window.location.reload();
-
-        //        // } else {
-        //        //     currentPageTemplate = $state.current.templateUrl;
-        //        //     console.log("reloading:" + currentPageTemplate);
-        //        //     $templateCache.remove(currentPageTemplate);
-        //          //   $state.reload();
-        //        // }
-
-        //     }
-        // });
     }
 ]);
