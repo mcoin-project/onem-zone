655097b0755e1dab4a7642939ec24e2ba1d7808a
Chris Horn <chrishornuk@gmail.com>
2019-02-15 18:49:49 +0000

    trying to put services obj back inside cache

--- app_client/js/services/cache.js
+++ app_client/js/services/cache.js
@@ -1,27 +1,27 @@
-const services = [
-    { name: ['onem'], icon: 'home', template: 'cards' },
-    { name: ['account'], icon: 'settings', template: 'cards' },
-    { name: ['subscribe'], icon: 'note_add', template: 'cards' },
-    { name: ['market'], icon: 'shopping_cart', template: 'cards', default: true },
-    { name: ['msg'], icon: 'message', template: 'cards' },
-    { name: ['xgroup'], icon: 'group_work', template: 'cards' },
-    { name: ['post'], icon: 'comment', template: 'cards' },
-    { name: ['wiki'], icon: 'import_contacts', template: 'cards' },
-    { name: ['france24'], icon: 'list', template: 'cards' },
-    { name: ['aljazeera'], icon: 'format_align_left', template: 'cards' },
-    { name: ['contacts'], icon: 'supervisor_account', template: 'cards' },
-    { name: ['reuters'], icon: 'description', template: 'cards' },
-    { name: ['unsubscribe'], icon: 'delete', template: 'cards' }
-];
-
-var mtResponse;
-
 
 ONEmSimModule.factory('Cache', [
     'Socket',
     '$timeout',
     '$interval',
     function (Socket, $timeout, $interval) {
+
+        var services = [
+            { name: ['onem'], icon: 'home', template: 'cards' },
+            { name: ['account'], icon: 'settings', template: 'cards' },
+            { name: ['subscribe'], icon: 'note_add', template: 'cards' },
+            { name: ['market'], icon: 'shopping_cart', template: 'cards', default: true },
+            { name: ['msg'], icon: 'message', template: 'cards' },
+            { name: ['xgroup'], icon: 'group_work', template: 'cards' },
+            { name: ['post'], icon: 'comment', template: 'cards' },
+            { name: ['wiki'], icon: 'import_contacts', template: 'cards' },
+            { name: ['france24'], icon: 'list', template: 'cards' },
+            { name: ['aljazeera'], icon: 'format_align_left', template: 'cards' },
+            { name: ['contacts'], icon: 'supervisor_account', template: 'cards' },
+            { name: ['reuters'], icon: 'description', template: 'cards' },
+            { name: ['unsubscribe'], icon: 'delete', template: 'cards' }
+        ];
+        
+        var mtResponse;
 
         const SMS_TIMEOUT = 10000;
 
@@ -221,6 +221,10 @@ ONEmSimModule.factory('Cache', [
 
         return {
 
+            services: services,
+
+            mtResponse: mtResponse,
+            
             getLandingService: function () {
                 var result;
                 for (var i = 0; i < services.length; i++) {
