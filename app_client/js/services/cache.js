c51b2934daefeb2bc714f0b5eccf8bc7b632a27b
Chris Horn <chrishornuk@gmail.com>
2019-02-15 10:19:10 +0000

    bug fix

--- app_client/js/services/cache.js
+++ app_client/js/services/cache.js
@@ -7,20 +7,6 @@ ONEmSimModule.factory('Cache', [
 
         const SMS_TIMEOUT = 10000;
 
-        const services = [
-            { name: ['onem'], icon: 'home', template: 'cards' },
-            { name: ['account'], icon: 'settings', template: 'cards' },
-            { name: ['subscribe'], icon: 'note_add', template: 'cards' },
-            { name: ['market'], icon: 'shopping_cart', template: 'cards', default: true },
-            { name: ['msg'], icon: 'message', template: 'cards' },
-            { name: ['xgroup'], icon: 'group_work', template: 'cards' },
-            { name: ['france24'], icon: 'list', template: 'cards' },
-            { name: ['aljazeera'], icon: 'format_align_left', template: 'cards' },
-            { name: ['contacts'], icon: 'supervisor_account', template: 'cards' },
-            { name: ['reuters'], icon: 'description', template: 'cards' },
-            { name: ['unsubscribe'], icon: 'delete', template: 'cards' }
-        ];
-
         var mtResponse;
         var timer;
 
@@ -32,6 +18,20 @@ ONEmSimModule.factory('Cache', [
         };
 
         var processServicesList = function (mtText) {
+
+            const services = [
+                { name: ['onem'], icon: 'home', template: 'cards' },
+                { name: ['account'], icon: 'settings', template: 'cards' },
+                { name: ['subscribe'], icon: 'note_add', template: 'cards' },
+                { name: ['market'], icon: 'shopping_cart', template: 'cards', default: true },
+                { name: ['msg'], icon: 'message', template: 'cards' },
+                { name: ['xgroup'], icon: 'group_work', template: 'cards' },
+                { name: ['france24'], icon: 'list', template: 'cards' },
+                { name: ['aljazeera'], icon: 'format_align_left', template: 'cards' },
+                { name: ['contacts'], icon: 'supervisor_account', template: 'cards' },
+                { name: ['reuters'], icon: 'description', template: 'cards' },
+                { name: ['unsubscribe'], icon: 'delete', template: 'cards' }
+            ];
 
             var activeServices = [];
 
