f0454bb8cfa1c16b81e0b8a9ea10cb60d1024c8d
Chris Horn <chrishornuk@gmail.com>
2019-02-15 19:06:38 +0000

    services appears to be working

---
+++ app_client/js/constants/services.js
@@ -0,0 +1,17 @@
+ONEmSimModule.constant('Services', 
+    [
+        { name: ['onem'], icon: 'home', template: 'cards' },
+        { name: ['account'], icon: 'settings', template: 'cards' },
+        { name: ['subscribe'], icon: 'note_add', template: 'cards' },
+        { name: ['market'], icon: 'shopping_cart', template: 'cards', default: true },
+        { name: ['msg'], icon: 'message', template: 'cards' },
+        { name: ['xgroup'], icon: 'group_work', template: 'cards' },
+        { name: ['post'], icon: 'comment', template: 'cards' },
+        { name: ['wiki'], icon: 'import_contacts', template: 'cards' },
+        { name: ['france24'], icon: 'list', template: 'cards' },
+        { name: ['aljazeera'], icon: 'format_align_left', template: 'cards' },
+        { name: ['contacts'], icon: 'supervisor_account', template: 'cards' },
+        { name: ['reuters'], icon: 'description', template: 'cards' },
+        { name: ['unsubscribe'], icon: 'delete', template: 'cards' }
+    ]
+);
