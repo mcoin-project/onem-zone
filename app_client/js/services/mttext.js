c890d537ad372cde5d0b05b9aa3af5549b99adf8
Chris Horn <chrishornuk@gmail.com>
2019-10-12 15:18:43 +0100

    fixing option recognition

--- app_client/js/services/mttext.js
+++ app_client/js/services/mttext.js
@@ -155,7 +155,7 @@ ONEmSimModule.factory('MtText', function () {
 		var self = this;
 
 		var optionFormats = [
-			/^([A-Z]) ([@*'A-Z#a-z0-9].+)/gm,  // check for basic option A-Z
+			/^([A-Z]) ([+@*'A-Z#a-z0-9].+)/gm,  // check for basic option A-Z
 			/^(\d+) (['+@*A-Z#a-z0-9].+)/gm,   // check for basic numbering options 0-9
 			/^(\d+[\.\d]+) ([+@*A-Z#a-z0-9].+)/gm  // check for wiki-style section numbering
 		];
